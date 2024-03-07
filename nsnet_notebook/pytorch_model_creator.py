446cc85685fb198cc46781b165ec7c85a5af0a68
alecerio <alessandrocerioliphd@gmail.com>
2024-03-07 15:27:34 +0100

    parametrize hidden size

--- nsnet_notebook/pytorch_model_creator.py
+++ nsnet_notebook/pytorch_model_creator.py
@@ -2,16 +2,16 @@ import torch
 import torch.nn as nn
 
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size=400):
         super(Model, self).__init__()
 
-        self.fc1 = nn.Linear(in_features=257, out_features=400, bias=True)  # Set bias=False to separate MatMul and Add
-        self.fc3 = nn.Linear(in_features=400, out_features=600, bias=True)  # Set bias=False to separate MatMul and Add
+        self.fc1 = nn.Linear(in_features=257, out_features=hidden_size, bias=True)  # Set bias=False to separate MatMul and Add
+        self.fc3 = nn.Linear(in_features=hidden_size, out_features=600, bias=True)  # Set bias=False to separate MatMul and Add
         self.fc4 = nn.Linear(in_features=600, out_features=600, bias=True)  # Set bias=False to separate MatMul and Add
         self.fc2 = nn.Linear(in_features=600, out_features=257, bias=True)  # Set bias=False to separate MatMul and Add
 
-        self.gru1 = nn.GRU(input_size=400, hidden_size=400, num_layers=1, batch_first=True)
-        self.gru2 = nn.GRU(input_size=400, hidden_size=400, num_layers=1, batch_first=True)
+        self.gru1 = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, num_layers=1, batch_first=True)
+        self.gru2 = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, num_layers=1, batch_first=True)
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
@@ -41,23 +41,19 @@ class Model(nn.Module):
 
         return x, h1, h2
 
-# Instantiate the model
-model = Model()
-
-# Print the model architecture
-print(model)
-
 import torch.onnx
 
+hidden_size = 25
+
 # Instantiate the model
-model = Model()
+model = Model(hidden_size)
 
 # Create dummy input tensor
 dummy_input = torch.randn(1, 1, 257)
 
 # Create initial hidden states
-h1 = torch.zeros(1, 1, 400)
-h2 = torch.zeros(1, 1, 400)
+h1 = torch.zeros(1, 1, hidden_size)
+h2 = torch.zeros(1, 1, hidden_size)
 
 # Export the model to ONNX format
 onnx_path = "model2.onnx"
